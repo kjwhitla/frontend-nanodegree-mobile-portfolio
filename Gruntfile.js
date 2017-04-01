4311891ff23ecfa13a13840acf53d4663a736025
Keegan Whitla <kjwhitla@gmail.com>
2017-04-01 10:04:29 -0700

    feat: Gruntfile.js pagespeed insights and ngrok Added feature gets a pagespeed insights score for the site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,12 @@
 // ngrok & pagespeed insights capability per James Cryer
 // http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
 
-module.exports = function(grunt) {
+var ngrok = require('ngrok');
 
+module.exports = function(grunt) {
+    
+     // Load grunt tasks
+    require('load-grunt-tasks')(grunt);
 
     // 1. All configuration goes here
     grunt.initConfig({
@@ -25,18 +29,49 @@ module.exports = function(grunt) {
                     dest: 'img/build/'
                 }]
             }
-        }
+        },
 
         // Add task to use pagespeed insights
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold: 90
+            },
+            local: {
+                options: {
+                    strategy: "desktop"
+                }
+            },
+            mobile: {
+                options: {
+                  strategy: "mobile"
+                }
+            }
+        }
+    });
+        
+        grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+        var done = this.async();
+        var port = 8080;
+
+        ngrok.connect(port, function(err, url) {
+            if (err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+            grunt.config.set('pagespeed.options.url', url);
+            grunt.task.run('pagespeed');
+            done();
+        });
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-pagespeed');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['uglify', 'imagemin']);
+    grunt.registerTask('default', ['uglify', 'imagemin', 'psi-ngrok']);
 
 };
